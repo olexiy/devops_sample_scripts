b123f795e7e86cb1158c73de63f9977ee2639c13
Olexiy <olexiy.sokurenko@gmail.com>
2021-06-23 18:16:52 +0200

    Zerro Downtime Webserver example

---
+++ Terraform/TF_from_0/Lesson-16_data_Sources/data_sources.tf
@@ -0,0 +1,106 @@
+provider "aws" {
+    region = "eu-central-1"
+}
+
+
+data "aws_availability_zones" "working" {}
+data "aws_caller_identity" "current" {}
+data "aws_region" "current" {}
+data "aws_vpcs" "my_vpcs" {}
+
+
+data "aws_vpc" "prod_vpc" { }
+
+data "aws_ami" "latest_amazon_linux" {
+  owners      = ["amazon"]
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+data "aws_ami" "latest_ubuntu" {
+  owners      = ["099720109477"]
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+  }
+}
+
+data "aws_ami" "latest_windows_2016" {
+  owners      = ["amazon"]
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["Windows_Server-2016-English-Full-Base-*"]
+  }
+
+}
+
+
+// How to use
+/*
+resource "aws_instance" "my_webserver_with_latest_ubuntu_ami" {
+  ami           = data.aws_ami.latest_ubuntu.id
+  instance_type = "t3.micro"
+}
+*/
+
+
+output "latest_windows_2016_ami_id" {
+  value = data.aws_ami.latest_windows_2016.id
+}
+
+output "latest_windows_2016_ami_name" {
+  value = data.aws_ami.latest_windows_2016.name
+}
+
+
+output "latest_amazon_linux_ami_id" {
+  value = data.aws_ami.latest_amazon_linux.id
+}
+
+output "latest_amazon_linux_ami_name" {
+  value = data.aws_ami.latest_amazon_linux.name
+}
+
+
+output "latest_ubuntu_ami_id" {
+  value = data.aws_ami.latest_ubuntu.id
+}
+
+output "latest_ubuntu_ami_name" {
+  value = data.aws_ami.latest_ubuntu.name
+}
+
+output "prod_vpc_id" {
+  value = data.aws_vpc.prod_vpc.id
+}
+
+output "prod_vpc_cidr" {
+  value = data.aws_vpc.prod_vpc.cidr_block
+}
+
+output "aws_vpcs" {
+  value = data.aws_vpcs.my_vpcs.ids
+}
+
+
+output "data_aws_availability_zones" {
+  value = data.aws_availability_zones.working.names
+}
+
+
+output "data_aws_caller_identity" {
+  value = data.aws_caller_identity.current.account_id
+}
+
+output "data_aws_region_name" {
+  value = data.aws_region.current.name
+}
+
+output "data_aws_region_description" {
+  value = data.aws_region.current.description
+}
